9d413271d1fa3c12ce028d02bff495a54964500f
caesar-dotcom <aworcaesar@gmail.com>
2026-08-27 18:05:34 +0100

    UPDATE 2

--- server/server.js
+++ server/server.js
@@ -157,7 +157,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // Explicit OPTIONS handler
-app.options("*", cors(corsOptions));
+app.options(cors(corsOptions));
 
 // =========================================
 // BODY PARSER
